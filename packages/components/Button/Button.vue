f4614755a590b7460cdee7ae2563b5e96cdcb396
pepedd864 <pepedd@qq.com>
2024-11-03 23:53:15 +0800

    chore: 引入vitest

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+import type { ButtonProps } from './type';
+import { ref } from 'vue';
+
 defineOptions({
   name: 'HpButton',
 });
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: 'button',
+  nativeType: 'button',
+});
+
+const slots = defineSlots();
+
+const _ref = ref<HTMLElement | null>(null);
 </script>
 
 <template>
-  <button class="hp-button">
-    <slot></slot>
-  </button>
+  <component
+    :is="tag"
+    ref="_ref"
+    :type="tag === 'button' ? nativeType : 0"
+    :disabled="disabled"
+    :class="['hp-button', props.type]"
+  >
+  </component>
 </template>
