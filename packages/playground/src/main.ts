2a7845f06e1cdf50813f672e23009e3ef6a2c778
pepedd864 <pepedd@qq.com>
2024-11-05 16:41:46 +0800

    chore: 项目打包配置

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import ArchUI from '@arch-design/arch-ui';
+import '@arch-design/arch-ui/dist/index.css'
 
 createApp(App).use(ArchUI).mount('#app');
