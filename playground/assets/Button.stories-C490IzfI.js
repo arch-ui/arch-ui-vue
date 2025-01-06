b657ded860ac3856514bd33de75ba488eaa758c0
pepedd864 <pepedd864@users.noreply.github.com>
2025-01-06 12:27:13 +0000

    deploy: e9d2290d794416776a1fc9993b957f2bf097f053

--- playground/assets/Button.stories-C490IzfI.js
+++ playground/assets/Button.stories-C490IzfI.js
@@ -1,4 +1,4 @@
-import{w as f,u as v,e as z}from"./index-BflBC_Ux.js";import"./index-BRXAtGvy.js";import{M as o,Q as w}from"./Button.vue-DAGarFHk.js";import"./vue.esm-bundler-D4nsrp9Q.js";const E={title:"Components/Button",tags:["autodocs"],argTypes:{type:{control:{type:"select"},options:["primary","secondary","outline","text"]},status:{control:{type:"select"},options:["default","success","warning","danger","info"]},size:{control:{type:"select"},options:["large","default","small","mini"]},disabled:{control:"boolean"},loading:{control:"boolean"},tag:{control:{type:"select"},options:["button","a","div"]},htmlType:{control:{type:"select"},options:["button","submit","reset"]},icon:{control:{type:"text"}},loadingIcon:{control:{type:"text"}}}},s=t=>`
+import{w as v,u as f,e as z}from"./index-BflBC_Ux.js";import"./index-DiukUMmR.js";import{M as o,Q as w}from"./Button.vue-DOVYKMB_.js";import"./vue.esm-bundler-DWx66a-0.js";const C={title:"Components/Button",tags:["autodocs"],argTypes:{type:{control:{type:"select"},options:["primary","secondary","outline","text"]},status:{control:{type:"select"},options:["default","success","warning","danger","info"]},size:{control:{type:"select"},options:["large","default","small","mini"]},disabled:{control:"boolean"},loading:{control:"boolean"},tag:{control:{type:"select"},options:["button","a","div"]},htmlType:{control:{type:"select"},options:["button","submit","reset"]},icon:{control:{type:"text"}},loadingIcon:{control:{type:"text"}}}},s=t=>`
 <div style="margin:5px">
   ${t}
 </div>
@@ -12,10 +12,10 @@ import{w as f,u as v,e as z}from"./index-BflBC_Ux.js";import"./index-BRXAtGvy.js
                 :icon="args.icon"
       >
         <template #default>
-          {{args.default}}
+          <div v-html="args.default"></div>
         </template>
       </a-button>
-      `)}),play:async({canvasElement:t,args:b,step:y})=>{const B=f(t);await y("click btn",async()=>{await v.click(B.getByRole("button"))}),z(b.onClick).toHaveBeenCalled()}},a={args:{loading:!0},render:t=>({components:{AButton:o},setup(){return{args:t}},template:s(`
+      `)}),play:async({canvasElement:t,args:b,step:y})=>{const B=v(t);await y("click btn",async()=>{await f.click(B.getByRole("button"))}),z(b.onClick).toHaveBeenCalled()}},a={args:{loading:!0},render:t=>({components:{AButton:o},setup(){return{args:t}},template:s(`
       <a-button :loading="args.loading">图标按钮</a-button>
       `)})},e={args:{},render:t=>({components:{AButton:o,AButtonGroup:w},setup(){return{args:t}},template:s(`<a-button-group 
                 :type="args.type" 
@@ -51,7 +51,7 @@ import{w as f,u as v,e as z}from"./index-BflBC_Ux.js";import"./index-BRXAtGvy.js
                 :icon="args.icon"
       >
         <template #default>
-          {{args.default}}
+          <div v-html="args.default"></div>
         </template>
       </a-button>
       \`)
@@ -84,7 +84,7 @@ import{w as f,u as v,e as z}from"./index-BflBC_Ux.js";import"./index-BRXAtGvy.js
       <a-button :loading="args.loading">图标按钮</a-button>
       \`)
   })
-}`,...(p=(c=a.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var g,d,m;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+}`,...(p=(c=a.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var d,g,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
   args: {},
   render: (args: any) => ({
     components: {
@@ -109,4 +109,4 @@ import{w as f,u as v,e as z}from"./index-BflBC_Ux.js";import"./index-BRXAtGvy.js
         <a-button round>13141</a-button>
       </a-button-group>\`)
   })
-}`,...(m=(d=e.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const G=["Default","ButtonIcon","ButtonGroup"];export{e as ButtonGroup,a as ButtonIcon,n as Default,G as __namedExportsOrder,E as default};
+}`,...(m=(g=e.parameters)==null?void 0:g.docs)==null?void 0:m.source}}};const E=["Default","ButtonIcon","ButtonGroup"];export{e as ButtonGroup,a as ButtonIcon,n as Default,E as __namedExportsOrder,C as default};
